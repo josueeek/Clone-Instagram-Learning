be468c45279df3d5c6d9a3c220935f40152ae45f
Josue Jhonatas Calazans Pereira <josue.pereira@NOTELOC59.stspo.local>
2020-10-09 21:11:36 -0300

    Upload files

---
+++ src/pages/Feed/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#F5F5F5',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 24,
+        textAlign: 'center',
+        margin: 10,
+    },
+});
+
+export default function Feed() {
+    return (
+        <>
+            <View style={styles.container}>
+                <Text style={styles.title}>Hello Feed</Text>
+            </View>
+        </>
+    );
+}
