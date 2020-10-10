311ba3c371be2cce9e4eefa9d6c920869e7f031f
Josue Jhonatas Calazans Pereira <josue.pereira@NOTELOC59.stspo.local>
2020-10-10 00:42:19 -0300

    finalized

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { FlatList } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#F5F5F5',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    title: {
-        fontSize: 24,
-        textAlign: 'center',
-        margin: 10,
-    },
-});
+import FeedImage from '../../components/FeedImage';
+
+import api from '../../services/api';
+
+import {
+    Container,
+    Post,
+    Header,
+    Avatar,
+    Name,
+    Description,
+    Loading,
+} from './styles';
 
 export default function Feed() {
+    const [feed, setFeed] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [refreshing, setRefreshing] = useState(false);
+
+    async function loadPage() {
+        if (loading) return;
+
+        setLoading(true);
+
+        const { data } = await api.get(`/feed?_expand=author`);
+
+        setLoading(false);
+
+        setFeed(data);
+    }
+
+    async function refreshList() {
+        setRefreshing(true);
+
+        await loadPage();
+
+        setRefreshing(false);
+    }
+
+    useEffect(() => {
+        loadPage();
+    }, []);
+
     return (
-        <>
-            <View style={styles.container}>
-                <Text style={styles.title}>Hello Feed</Text>
-            </View>
-        </>
+        <Container>
+            <FlatList
+                key="list"
+                data={feed}
+                keyExtractor={(item) => String(item.id)}
+                viewabilityConfig={{
+                    viewAreaCoveragePercentThreshold: 10,
+                }}
+                showsVerticalScrollIndicator={false}
+                onRefresh={refreshList}
+                refreshing={refreshing}
+                ListFooterComponent={loading && <Loading />}
+                renderItem={({ item }) => (
+                    <Post>
+                        <Header>
+                            <Avatar source={{ uri: item.author.avatar }} />
+                            <Name>{item.author.name}</Name>
+                        </Header>
+
+                        <FeedImage
+                            aspectRatio={item.aspectRatio}
+                            smallSource={{ uri: item.image }}
+                            source={{ uri: item.small }}
+                        />
+
+                        <Description>
+                            <Name>{item.author.name}</Name> {item.description}
+                        </Description>
+                    </Post>
+                )}
+            />
+        </Container>
     );
 }
